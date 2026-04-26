944d880b1d11c94b2632da5f4ff44f4ea14c17ff
Luca <williamlucacosta17@gmail.com>
2026-04-26 12:59:21 +0200

    feat: SEO setup - history mode, meta tags, sitemap, robots e redirect SPA per GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -46,8 +46,11 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+    history: createWebHistory(),
+    routes,
+    scrollBehavior() {
+        return { top: 0 }
+    }
 })
 
 export default router
